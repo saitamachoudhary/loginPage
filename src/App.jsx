9eb015fea021aabf7bb6f2392f2db947df6e9b58
saitamachoudhary <165796559+saitamachoudhary@users.noreply.github.com>
2024-08-01 04:40:00 -0700

    new files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className="ParentStyling">
+      <h1 className="h1Styling">Custom React App</h1>
+    </div>
+  );
+};
+
+export default App;
