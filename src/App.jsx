c9f31ca3d855d553df89778b7888a33d5906fe75
saitamachoudhary <165796559+saitamachoudhary@users.noreply.github.com>
2024-08-01 22:46:32 -0700

    02augfirstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import Login from "./Components/LoginPage";
 const App = () => {
+   const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Login/>
+    },
+    {
+      path:'/Dashboard',
+      element:<Dashboard/>
+    },
+    {
+      basename: "loginPage", // Set the basename to match your GitHub repo name
+    }
+   ])
   return (
     <div className="ParentStyling">
-      <h1 className="h1Styling">Custom React App</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 };
